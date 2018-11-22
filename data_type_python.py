722ef72cfba1c8b31470bee55eb8c19c82e74a11
Johan Suarez <jhoanseb1992@gmail.com>
2018-11-22 14:33:06 +1100

    Create data_type_python.py

---
+++ data_type_python.py
@@ -0,0 +1,24 @@
+## Primitive data
+#lib of date
+from datetime import datatime
+
+integer_type = 42
+float_type = 3.1416
+bool_type = True
+hex_type = 0xff
+oct_type = 0o23
+today = datatime.now()
+str_type = 'johan'
+
+## Structured data
+from IPython.display import Image
+
+Image('mysql_table_example.png')
+
+## Semi-Structured data
+
+Image('html_markup_exmaple.png')
+
+## No structured
+
+# just text
